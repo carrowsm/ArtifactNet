0c76f97bfe3343c6e94010b51dacf404ca7293ac
Colin <arrowsmith.colin@gmail.com>
2020-07-31 16:50:26 -0400

    Removed workaround for hparam logger

--- util/loggers.py
+++ util/loggers.py
@@ -1,12 +1,12 @@
-
-# from pytorch_lightning.logging import LightningLoggerBase, rank_zero_only, TensorBoardLogger
+import os
+import json
+import numpy as np
+import matplotlib.pyplot as plt
 
 from pytorch_lightning.utilities import rank_zero_only
 from pytorch_lightning.loggers import LightningLoggerBase
 from pytorch_lightning.logging import TensorBoardLogger
-import os
-import json
-import matplotlib.pyplot as plt
+
 
 
 
@@ -15,18 +15,17 @@ class TensorBoardCustom(TensorBoardLogger):
     """Custom changes to PyTorch Lightning's TensorBoardLogger."""
     def __init__(self, save_dir, name='default', save_hparams=False):
         super(TensorBoardCustom, self).__init__(save_dir, name=name, version=None)
-        # self.logdir = save_dir
         self.save_hparams = save_hparams
-        # self.name = name
 
 
-    @rank_zero_only
-    def log_hyperparams(self, params):
-        # Bypass this as it does not currently work
-        if self.save_hparams :
-            return
-        else :
-            return
+    # @rank_zero_only
+    # def log_hyperparams(self, params):
+    #     # Bypass this as it does not currently work
+    #     if self.save_hparams :
+    #         return
+    #     else :
+    #         return
+
 
     @rank_zero_only
     def log_metrics(self, metrics, step):
@@ -46,22 +45,15 @@ class TensorBoardCustom(TensorBoardLogger):
                     # for this to work, the key should have format d_loss or g_loss
                     for k_i, v_i in v.items() :
                         d[k_i] = v_i
-                        # self.experiment.add_scalar(f"{plot_name}/{k_i}", v_i, step)
                 else :
                     d[k] = v
-                    # self.experiment.add_scalar(f"{plot_name}/{k}", v, step)
 
         # Add both training and validation metrics to logger
         self.experiment.add_scalars("trg_loss/", trg_metrics, step)
         self.experiment.add_scalars("val_loss/", val_metrics, step)
 
 
-
-
-
-
-
-    def add_mpl_img(self, tag, X, step) :
+    def add_mpl_img(self, tag, X, step, clip_vals=False) :
         """ Creates a matplotlib image out of a 4D tensor or list of
         2D tensors:
         X.shape == (N, 1, D, H, W) or
@@ -78,6 +70,8 @@ class TensorBoardCustom(TensorBoardLogger):
                 titles = np.arange(0, N, 1).astype(str)
             fig, ax = plt.subplots(nrows=1, ncols=N, figsize=[6*N, 6])
             for i in range(N) :
+                if clip_vals :
+                    X[i] = np.clip(X[i], -1.0, 1.0)
                 img = ax[i].imshow(X[i], cmap=cm[1])
                 ax[i].set_title(titles[i])
                 fig.colorbar(img, ax=ax[i], shrink=0.6)
