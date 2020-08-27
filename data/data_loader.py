17e3dcb751583cd5b25dd1d2a488b4f9b747e316
Colin <arrowsmith.colin@gmail.com>
2020-08-27 13:25:58 -0400

    Created base dataloader class that reads from raw data and caches images

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,12 +1,16 @@
 import os
 import sys
+from typing import Callable, Optional, Tuple
+
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
+from skimage.filters import threshold_otsu
+from scipy import ndimage
 
 import torch
-import torch.utils.data as t_data
+from torch.utils.data import Dataset
 import torchvision
 
 from data.sitk_processing import read_dicom_image, resample_image, read_nrrd_image
@@ -20,14 +24,14 @@ def load_image_data_frame(path, img_X, img_Y, label_col="has_artifact") :
     ------------
     path (str) :    Full path to the CSV containing the image IDs and corresponding
                     labels.
-    X_label (str) : The CSV label for images in domain X. These labels are in the
+    X_label (list) : The CSV label for images in domain X. These labels are in the
                     column col_name in the CSV.
-    Y_label (str) : The CSV label for images in domain Y.
+    Y_label (list) : The CSV label for images in domain Y.
     label_col (str): The CSV column name containing the image labels. Default is
                     'has_artifact'.
     Returns :
     ---------
-    One list for each image domain with the z-index of the DA slice for each patient.
+    One DF for each image domain with the z-index of the DA slice for each patient.
     """
     df = pd.read_csv(path,
                      usecols=["patient_id", label_col, "DA_z"],
@@ -41,7 +45,160 @@ def load_image_data_frame(path, img_X, img_Y, label_col="has_artifact") :
     print(X_df["has_artifact"])
     print(Y_df["has_artifact"])
 
-    return X_df["DA_z"], Y_df["DA_z"]
+    return X_df, Y_df
+
+
+
+class BaseDataset(Dataset):
+    """Dataset class used to load images (paired or unpaired) to dental artifact
+    reduction GANs.
+
+    DICOM images are loaded using SimpleITK and cached in NRRD or NPY format for
+    faster loading during training.
+    """
+    def __init__(self,
+                 X_df: pd.DataFrame, Y_df: pd.DataFrame,
+                 img_dir: str,
+                 cache_dir: str,
+                 file_type: str = "DICOM",
+                 image_size=[8, 256, 256],
+                 dim=3,
+                 transform: Optional[Callable] = None,
+                 num_workers: int = 1,
+                 patient_id_col: str = "patient_id",
+                 da_size_col: str = "has_artifact",
+                 da_slice_col: str = "a_slice") :
+        """ Initialize the class.
+
+        Get the full paths to all images. Load and preprocess if the files if
+        the cache does not already exist.
+
+        Parameters
+        ----------
+        X_df : pandas.DataFrame
+            A pandas dataframe containing the DA z-index, indexed by patient ID,
+            of each image in domain X.
+        Y_df : pandas.DataFrame
+            A pandas dataframe containing the DA z-index, indexed by patient ID,
+            of each image in domain Y.
+        img_dir : str
+            The path to the directory containing the raw image files.
+        cache_dir :
+            The path to the directory in which to cache preprocessed images.
+        file_type: str, (default: "nrrd")
+            The file type to load. Can be either "npy" or "nrrd" or "dicom".
+        image_size : int, list, None
+            The size of the image to use (same for both domains X and Y).
+            - If None, use the entire image (centre_pix will be ignored).
+            - If int=a, each image will be cropped to form a cube of size
+              a^3 aroud centre_pix.
+            - If list, it should have len=3 and represent the (z, y, x) size to
+              crop all images to.
+        dim : int
+            The dimension of the output image. If 2, images will have shape
+            If 2, the images will have shape(batch_size, z_size, y_size, x_size).
+            If 3, the images will have shape
+            (batch_size, 1, z_size, y_size, x_size).
+        transform
+            Callable used to transform the images after loading and preprocessing.
+        num_workers : int
+            The number of parallel processes to use for image preprocessing.
+        patient_id_col: str (default: "patient_id")
+            The name of the column lif containing the patient_id.
+        da_size_col: str (default: "has_artifact")
+            The name of the column containing the magnitude of the DA.
+        da_slice_col: str  (default: a_slice)
+            The name of the column containing the z-index of the DA.
+        """
+        self.X_df, self.Y_df = X_df, Y_df
+        self.img_dir = img_dir
+        self.cache_dir = cache_dir
+        self.file_type = file_type
+        self.use_raw = use_raw
+        self.img_size = np.array(img_size)
+        self.dim = dim
+        self.transform = transform
+        self.num_workers = num_workers
+        self.patient_id_col = patient_id_col
+        self.da_size_col = da_size_col
+        self.da_slice_col = da_slice_col
+
+        # Get the correct function to load the raw image type
+        self.load_img = self.get_img_loader()
+
+        # Create a cache if needed. Check if cache already exists
+        sample_path = os.path.join(self.cache_dir, X_df.index[0] + ".nrrd")
+        if os.path.exists(sample_path) :
+            # The sample file exists. Now check that the size is right
+            sample_file = sitk.GetArrayFromImage(read_nrrd_image(sample_path))
+            if np.array(sample_file.shape) != self.img_size :
+                # The images are not correctly cached. Process them again
+                self._prepare_data()
+        else :
+            # The images are not cached. Process them.
+            os.makedirs(self.cache_dir)
+            self._prepare_data()
+
+
+    def _get_img_loader(self) :
+        if self.file_type == "npy" :
+            return np.load
+        elif self.file_type == "nrrd" :
+            return read_nrrd_image
+        elif self.file_type == "dicom" :
+            return read_dicom_image
+        else :
+            raise Exception(f"file_type {self.file_type} not accepted.")
+
+
+    def _prepare_data(self) :
+        """Preprocess and cache the dataset."""
+        self.full_df = pd.concat([self.X_df, self.Y_df])
+        Parallel(n_jobs=self.num_workers)(
+            delayed(self._preprocess_image)(patient_id)
+            for patient_id in self.full_df.index)
+
+
+    def _preprocess_image(self, patient_id: str) :
+        """Preprocess and cache a single image."""
+        # Load image and DA index in original voxel spacing
+        path = os.path.join(self.img_dir, patient_id)
+        image = read_dicom_image(path)
+        da_idx = int(self.full_df.at[patient_id, self.da_slice_col])
+
+        # Resample image and DA slice to [1,1,1] voxel spacing
+        da_coords = image.TransformIndexToPhysicalPoint([150, 150, da_idx]) # Physical coords of DA
+        image = resample_image(image, new_spacing=[1, 1, 1])
+        da_z = image.TransformPhysicalPointToIndex(da_coords)[2] # Index of DA in isotropic spacing
+
+        ### Cropping ###
+        # Get the centre of the head in the DA slice
+        slice = sitk.GetArrayFromImage(image[:, :, da_z])
+        t = threshold_otsu(np.clip(slice, -1000, 1000))
+        slice = np.array(slice > t, dtype=int)
+        com  = ndimage.measurements.center_of_mass(slice)
+        y, x = int(com[0]) - 25, int(com[1])
+        crop_centre = np.array([x, y, da_z])
+
+        # Crop to required size around this point
+        _min = np.floor(crop_centre - self.img_size / 2).astype(np.int64)
+        _max = np.floor(crop_centre + self.img_size / 2).astype(np.int64)
+        image = image[_min[0] : _max[0], _min[1] : _max[1], _min[2] : _max[2]]
+        ### --------- ###
+
+        # Save the image
+        sitk.WriteImage(image, os.path.join(self.cache_path, f"{patient_id}.nrrd))
+
+
+    def __getitem__(self, index) :
+        raise NotImplementedError
+
+    def __len__(self) :
+        raise NotImplementedError
+
+
+
+
 
 
 
@@ -122,7 +279,7 @@ def load_img_names(dir, y_da_df=None, n_da_df=None, f_type="npy", suffix="", dat
 
 
 
-class UnpairedDataset(t_data.Dataset):
+class UnpairedDataset(Dataset):
     """
     Image dataset for ArtifactNet artifact removal GAN.
 
--- data/sitk_processing.py
+++ data/sitk_processing.py
@@ -16,35 +16,104 @@ def get_dicom_path(path) :
               return os.path.join(root, name)
 
 
-def resample_image(image, new_spacing=[1,1,1]):
+def resample_image(image: sitk.Image,
+             spacing: Union[float, Sequence[float], np.ndarray],
+             interpolation: str = "linear",
+             anti_alias: bool = True,
+             anti_alias_sigma: Optional[float] = None,
+             transform: Optional[sitk.Transform] = None,
+             output_size: Optional[Sequence[float]] = None) -> sitk.Image:
+    """Resample image to a given spacing, optionally applying a transformation.
 
-    #Resample image to common resolution of 1x1x1
-    # new_spacing = [1,1,1]
+    Parameters
+    ----------
+    image
+        The image to be resampled.
 
-    #Set up SitK resampling image filter
+    spacing
+        The new image spacing. If float, assumes the same spacing in all
+        directions. Alternatively, a sequence of floats can be passed to
+        specify spacing along each dimension. Passing 0 at any position will
+        keep the original spacing along that dimension (useful for in-plane
+        resampling).
+
+    interpolation, optional
+        The interpolation method to use. Valid options are:
+        - "linear" for bi/trilinear interpolation (default)
+        - "nearest" for nearest neighbour interpolation
+        - "bspline" for order-3 b-spline interpolation
+
+    anti_alias, optional
+        Whether to smooth the image with a Gaussian kernel before resampling.
+        Only used when downsampling, i.e. when `spacing < image.GetSpacing()`.
+        This should be used to avoid aliasing artifacts.
+
+    anti_alias_sigma, optional
+        The standard deviation of the Gaussian kernel used for anti-aliasing.
+
+    transform, optional
+        Transform to apply to input coordinates when resampling. If None,
+        defaults to identity transformation.
+
+    output_size, optional
+        Size of the output image. If None, it is computed to preserve the
+        whole extent of the input image.
+
+    Returns
+    -------
+    sitk.Image
+        The resampled image.
+    """
+    INTERPOLATORS = {
+        "linear": sitk.sitkLinear,
+        "nearest": sitk.sitkNearestNeighbor,
+        "bspline": sitk.sitkBSpline,
+    }
+
+    try:
+        interpolator = INTERPOLATORS[interpolation]
+    except KeyError:
+        raise ValueError(
+            f"interpolator must be one of {list(INTERPOLATORS.keys())}, got {interpolator}."
+        )
+
+    original_spacing = np.array(image.GetSpacing())
+    original_size = np.array(image.GetSize())
+
+    if isinstance(spacing, (float, int)):
+        new_spacing = np.repeat(spacing,
+                                len(original_spacing)).astype(np.float64)
+    else:
+        spacing = np.asarray(spacing)
+        new_spacing = np.where(spacing == 0, original_spacing, spacing)
+
+    if not output_size:
+        new_size = np.floor(original_size * original_spacing / new_spacing).astype(np.int)
+    else:
+        new_size = np.asarray(output_size)
+
     rif = sitk.ResampleImageFilter()
+    rif.SetOutputOrigin(image.GetOrigin())
     rif.SetOutputSpacing(new_spacing)
     rif.SetOutputDirection(image.GetDirection())
+    rif.SetSize(new_size.tolist())
 
-    #Get original image size and spacing
-    orig_size = np.array(image.GetSize(), dtype = np.int)
-    orig_spacing = np.array(image.GetSpacing())
+    if transform is not None:
+        rif.SetTransform(transform)
 
-    #Calculate new image size based on current size and desired spacing.
-    new_size = np.ceil(orig_size*(orig_spacing/new_spacing)).astype(np.int)
-    new_size = [int(s) for s in new_size]
+    downsample = new_spacing > original_spacing
+    if downsample.any() and anti_alias:
+        if not anti_alias_sigma:
+            # sigma computation adapted from scikit-image
+            # https://github.com/scikit-image/scikit-image/blob/master/skimage/transform/_warps.py
+            anti_alias_sigma = np.maximum(1e-11, (original_spacing / new_spacing - 1) / 2)
+        sigma = np.where(downsample, anti_alias_sigma, 1e-11)
+        image = sitk.SmoothingRecursiveGaussian(image, sigma)
 
-    #Set up SitK resampling parameters
-    rif.SetSize(new_size)
-    rif.SetOutputOrigin(image.GetOrigin())
-    rif.SetOutputPixelType(image.GetPixelID())
-    rif.SetInterpolator(sitk.sitkLinear)
+    rif.SetInterpolator(interpolator)
+    resampled_image = rif.Execute(image)
 
-    #Resample image and generate numpy array from image
-    resampledImage = rif.Execute(image)
-    imageArray = sitk.GetArrayFromImage(resampledImage)
-
-    return imageArray
+    return resampled_image
 
 
 
