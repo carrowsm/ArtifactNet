2ddb247e929c4d6a8df09d34fcf415c560867494
Colin <arrowsmith.colin@gmail.com>
2020-09-16 16:12:46 -0400

    add specific GPU type to bash scripts

--- scripts/train_suw_to_none.sh
+++ scripts/train_suw_to_none.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -t 2-00:00:00
-#SBATCH --mem=20G
+#SBATCH --mem=16G
 #SBATCH -J ArtifactNet
-#SBATCH -c 5
+#SBATCH -c 11
 #SBATCH -N 1
 #SBATCH --account=radiomics_gpu
 #SBATCH --partition=gpu_radiomics
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:v100:1
 #SBATCH --output=strongORweak_to_none.out
 #SBATCH --ntasks-per-node=1
 
@@ -24,18 +24,17 @@ path=/cluster/home/carrowsm/ArtifactNet/cycleGAN.py
 
 # Paths to data and logs
 csv_path="/cluster/home/carrowsm/ArtifactNet/datasets/train_labels.csv"
-img_path="/cluster/projects/radiomics/Temp/colin/isotropic_npy/images"
-log_path="/cluster/home/carrowsm/logs/artifact_net/remove/cycleGAN"
+img_path="/cluster/projects/radiomics/RADCURE-images/"
+log_path="/cluster/home/carrowsm/logs/cycleGAN"
+cache_path="/cluster/projects/radiomics/Temp/colin/isotropic_nrrd/"
 
 ### EDIT BELOW ###
 # Hyperparameters for training the model
-epochs=50                                # Number of epochs for training
-learn_rate=0.0002                        # Initial rate for the trainer
-batch_size=3                             # Batch size for trainer
-aug_factor=10                            # Number of times to augment each image
-num_gpus=1                               # Number of GPUs to use for training
-num_cpus=5                               # Number of workers for dataloaders
-num_filters=32                           # Number of input filters for the model
+learn_rate=0.0002                       # Initial rate for the trainer
+batch_size=10                           # Batch size for trainer
+num_gpus=1                              # Number of GPUs to use for training
+num_cpus=11                             # Number of workers for dataloaders
+num_filters=32                          # Number of input filters for the model
 ### ---------- ###
 
 echo 'Started python script.'
@@ -43,9 +42,9 @@ python $path \
 --csv_path $csv_path \
 --img_dir $img_path \
 --log_dir $log_path \
+--cache_dir $cache_path \
 --batch_size $batch_size \
 --lr $learn_rate \
---augmentation_factor $aug_factor \
 --n_gpus $num_gpus \
 --n_cpus $num_cpus \
 --n_filters $num_filters \
